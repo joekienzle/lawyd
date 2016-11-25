013a961ac00515f937a4892b50f658da1b3d02fe
joekienzle <jojohannes.kienzle@gmail.com>
2016-11-25 22:40:16 +0100

    Added intial spring boot files

---
+++ src/main/java/net/laywd/LaywdServerApplication.java
@@ -0,0 +1,12 @@
+package net.laywd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaywdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaywdServerApplication.class, args);
+	}
+}
