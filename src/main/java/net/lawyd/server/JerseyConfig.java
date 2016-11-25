78e215acdf36c59bb5a40e427ac3fef76308b01a
joekienzle <jojohannes.kienzle@gmail.com>
2016-11-26 00:17:58 +0100

    Add jersey and simple rest test

---
+++ src/main/java/net/lawyd/server/JerseyConfig.java
@@ -0,0 +1,12 @@
+package net.lawyd.server;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        register(TodoResource.class);
+    }
+}
