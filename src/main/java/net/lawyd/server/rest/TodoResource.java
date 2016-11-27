8738f9f71952e68bbe90fee96c0e6af2a95af697
joekienzle <jojohannes.kienzle@gmail.com>
2016-11-27 19:34:56 +0100

    Added possibility to create todo

--- src/main/java/net/lawyd/server/rest/TodoResource.java
+++ src/main/java/net/lawyd/server/rest/TodoResource.java
@@ -1,19 +1,32 @@
 package net.lawyd.server.rest;
 
+import net.lawyd.server.rest.model.TodoJson;
+import net.lawyd.server.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Path("todo")
 public class TodoResource {
 
+    @Autowired
+    private TodoService todoService;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAll() {
-        return "";
+    public List<TodoJson> getAll() {
+        return todoService.findAllTodos().stream().map(TodoJson::new).collect(Collectors.toList());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public TodoJson createTodo(TodoJson todoJson) {
+        return new TodoJson(todoService.saveTodo(todoJson.convertToDbEntity()));
     }
 }
--- src/test/java/net/lawyd/server/RestTest.java
+++ src/test/java/net/lawyd/server/RestTest.java
@@ -1,6 +1,11 @@
 package net.lawyd.server;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.lawyd.server.persistence.Todo;
+import net.lawyd.server.rest.model.TodoJson;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +15,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,12 +36,49 @@ public class RestTest {
     }
 
     @Test
-    public void getEmptyTodo() throws Exception {
-        given().
-                accept(MediaType.APPLICATION_JSON).
-                when().
-                get().
-                then().
-                statusCode(Response.Status.OK.getStatusCode());
+    public void crudTodo() throws Exception {
+        Gson gson = new Gson();
+
+        String getInitalTodosResponse =
+                given().accept(MediaType.APPLICATION_JSON).
+                        when().get().
+                        then().statusCode(Response.Status.OK.getStatusCode()).extract().body().asString();
+
+        Type todoListType = new TypeToken<Collection<Todo>>() {
+        }.getType();
+        List<Todo> todos = gson.fromJson(getInitalTodosResponse, todoListType);
+        assertNotNull(todos);
+        int initalSize = todos.size();
+
+        String name = "My todo";
+        String description = "Super important";
+        int priority = 1;
+        String createResponse =
+                given().contentType(ContentType.JSON).body(gson.toJson(new TodoJson(null, name, description, priority))).accept(MediaType.APPLICATION_JSON).
+                        when().post().
+                        then().statusCode(Response.Status.OK.getStatusCode()).extract().body().asString();
+
+        TodoJson todoJson = gson.fromJson(createResponse, TodoJson.class);
+        assertNotNull(todoJson);
+        assertNotNull(todoJson.getId());
+        assertEquals(name, todoJson.getName());
+        assertEquals(description, todoJson.getDescription());
+        assertEquals(priority, todoJson.getPriority());
+
+        String getCreatedTodosResponse =
+                given().accept(MediaType.APPLICATION_JSON).
+                        when().get().
+                        then().statusCode(Response.Status.OK.getStatusCode()).extract().body().asString();
+
+        List<Todo> todosAfterCreate = gson.fromJson(getCreatedTodosResponse, todoListType);
+        assertNotNull(todosAfterCreate);
+        assertEquals(initalSize + 1, todosAfterCreate.size());
+        final boolean[] todoFound = {false};
+        todosAfterCreate.forEach(todo -> {
+            if (todo.getId().equals(todoJson.getId())) {
+                todoFound[0] = true;
+            }
+        });
+        assertTrue(todoFound[0]);
     }
 }
