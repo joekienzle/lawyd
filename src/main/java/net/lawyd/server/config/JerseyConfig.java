e9ba67a646a0252ba21baa3235637c8e93364d4d
joekienzle <jojohannes.kienzle@gmail.com>
2016-11-28 21:29:32 +0100

    Added health check

--- src/main/java/net/lawyd/server/config/JerseyConfig.java
+++ src/main/java/net/lawyd/server/config/JerseyConfig.java
@@ -1,5 +1,6 @@
 package net.lawyd.server.config;
 
+import net.lawyd.server.rest.HealthResource;
 import net.lawyd.server.rest.TodoResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,6 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(TodoResource.class);
+        register(HealthResource.class);
     }
 }
