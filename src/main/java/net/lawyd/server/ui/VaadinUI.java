ced16746d23502e0764f40bc9a4b17466f716e9f
joekienzle <jojohannes.kienzle@gmail.com>
2016-11-30 21:05:49 +0100

    Added simple todo crud ui

--- src/main/java/net/lawyd/server/ui/VaadinUI.java
+++ src/main/java/net/lawyd/server/ui/VaadinUI.java
@@ -1,19 +1,26 @@
 package net.lawyd.server.ui;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Notification;
+import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 @Theme("valo")
 public class VaadinUI extends UI {
 
+    @Autowired
+    private SpringViewProvider viewProvider;
+
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+        Navigator navigator = new Navigator(this, this);
+        navigator.addProvider(viewProvider);
+        setNavigator(navigator);
+        navigator.navigateTo("todos");
     }
 }
 
